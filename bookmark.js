a0fde1e19addeed7bc6f04e7b5eed02521e9899b
EddieWen-Taiwan <m20021991y@gmail.com>
2016-03-10 21:10:53 +0800

    First Commit

---
+++ bookmark.js
@@ -0,0 +1,15 @@
+javascript:
+
+document.getElementsByName('entry.954463140')[0].value = '我的名字是';
+
+document.getElementsByName('entry.709241587')[0].value = '我的電話是';
+
+document.getElementsByName('entry.1858865286')[0].value = "因私人緊急事項 (看病、就醫、因要事而返家)致延誤返回";
+document.getElementsByName('entry.1858865286')[0].disabled = false;
+
+document.getElementsByName('entry.1200038893')[0].value = "21:30";
+
+document.getElementsByName('entry.560005917')[0].value = "本人於此聲明所言屬實，並備相關文件供日後查證";
+document.getElementsByName('entry.560005917')[0].disabled = false;
+
+document.getElementsByTagName('form')[0].submit();
