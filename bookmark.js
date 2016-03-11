83ba838ebf262aea7211901b96a3b570d46d15f3
EddieWen-Taiwan <m20021991y@gmail.com>
2016-03-11 15:14:41 +0800

    Alert user info before submit

--- bookmark.js
+++ bookmark.js
@@ -12,4 +12,6 @@ document.getElementsByName('entry.1200038893')[0].value = "21:30";
 document.getElementsByName('entry.560005917')[0].value = "本人於此聲明所言屬實，並備相關文件供日後查證";
 document.getElementsByName('entry.560005917')[0].disabled = false;
 
+alert(`晚歸填寫成功！\n\n${document.getElementsByName('entry.954463140')[0].value} / ${document.getElementsByName('entry.709241587')[0].value}`);
+
 document.getElementsByTagName('form')[0].submit();
